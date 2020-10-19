7f3462fce41bf81d5d6b2e069891ccc59b37eaba
Pedro Fontes <pvfontes1000@gmail.com>
2020-10-19 19:36:26 -0300

    detecting circle

--- res/iris.py
+++ res/iris.py
@@ -1,4 +1,5 @@
 import cv2 as cv
+import numpy as np
 
 class Iris:
     def __init__(self):
@@ -18,5 +19,17 @@ class Iris:
         ])
       
         return cv.filter2D(img,-1,sharp) 
-     
+
+    def clareia(self, img, T, M):
+        
+        imgf = img.copy()
+
+        row, col = img.shape
+
+        for i in range(row):
+            for j in range(col):
+                if img[i, j] < T:
+                    imgf[i, j] += M
+
+        return imgf
  
--- test.py
+++ test.py
@@ -5,7 +5,14 @@ img = cv.cvtColor(cv.imread('img_test/0005_012.bmp'), cv.COLOR_BGR2GRAY)
 
 img_treater = Iris()
 
-img_t = img_treater.blur(img)
-img_t = img_treater.equalize(img_t)
+img = img_treater.equalize(img)
+img = img_treater.blur(img)
+img = img_treater.sharpening(img)
 
-cv.imwrite('img_test/tratada.bmp', img_t)
+circles = cv.HoughCircles(img, cv.HOUGH_GRADIENT, 1.2, 100, maxRadius=20)
+
+print(circles)
+
+# img = img_treater.clareia(img, 100, 20)
+
+cv.imwrite('img_test/tratada.bmp', img)
